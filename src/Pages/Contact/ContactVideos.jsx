2dda9b9f0e4f062cfb2e025f31041e688f08fe90
Tushar Koli <Jobhunttk0707@gmail.com>
2024-12-13 11:01:33 +0530

    first commit

---
+++ src/Pages/Contact/ContactVideos.jsx
@@ -0,0 +1,13 @@
+export const ContactVideos = [
+  { id: 1, name: "Video 1", src: "/src/assets/AboutPage/1.mp4" },
+  { id: 2, name: "Video 2", src: "/src/assets/AboutPage/2.mp4" },
+  { id: 3, name: "Video 3", src: "/src/assets/AboutPage/3.mp4" },
+  { id: 4, name: "Video 4", src: "/src/assets/AboutPage/5.mp4" },
+  { id: 4, name: "Video 5", src: "/src/assets/AboutPage/6.mp4" },
+  { id: 4, name: "Video 6", src: "/src/assets/AboutPage/7.mp4" },
+  { id: 4, name: "Video 7", src: "/src/assets/AboutPage/8.mp4" },
+  { id: 4, name: "Video 8", src: "/src/assets/AboutPage/9.mp4" },
+  // Add more videos as needed
+];
+
+export default ContactVideos;
---
+++ src/Pages/Home/Destinations/DestinationList.jsx
@@ -0,0 +1,108 @@
+export const destinations = [
+  {
+    name: "Bangalore ",
+    media: "/src/assets/DestinationCards/Bangalore .mp4", // Image
+    description: "City of Tech"
+  },
+  {
+    name: "Taj Mahal",
+    media: "/src/assets/DestinationCards/TajMahal.mp4", // Video
+    description: "The symbol of love in Agra"
+  },
+  {
+    name: "Mumbai",
+    media: "/src/assets/DestinationCards/Mumbai.mp4", // Image
+    description: "Gateway of India, vibrant city life"
+  },
+  {
+    name: "Konkan",
+    media: "/src/assets/DestinationCards/kokan.mp4", // Image
+    description: "Scenic beaches and nature"
+  },
+  {
+    name: "Jaipur",
+    media: "/src/assets/DestinationCards/Jaipur.mp4", // Image
+    description: "Pink City, Amber Fort, Hawa Mahal"
+  },
+  {
+    name: "Goa",
+    media: "/src/assets/DestinationCards/Goa.mp4", // Image
+    description: "Beaches, night life, and Portuguese architecture"
+  },
+  {
+    name: "Varanasi",
+    media: "/src/assets/DestinationCards/Varanasi.mp4", // Image
+    description: "Ancient spiritual hub, ghats of the Ganges"
+  },
+  {
+    name: "Kerala Backwaters",
+    media: "/src/assets/DestinationCards/Kerala.mp4", // Image
+    description: "Houseboats, serene canals, lush green landscapes"
+  },
+  {
+    name: "Leh-Ladakh",
+    media: "/src/assets/DestinationCards/Leh-Ladakh.mp4", // Image
+    description: "Breathtaking landscapes, monasteries, high-altitude adventure"
+  },
+  {
+    name: "Rajasthan Desert",
+    media: "/src/assets/DestinationCards/Rajasthan.mp4", // Image
+    description: "Thar Desert, camel rides, sand dunes"
+  },
+  {
+    name: "Rishikesh",
+    media: "/src/assets/DestinationCards/Rishikesh.mp4", // Image
+    description: "Yoga capital of the world, adventure sports on the Ganges"
+  },
+  {
+    name: "Shimla",
+    media: "/src/assets/DestinationCards/Shimala.mp4", // Image
+    description: "Hill station, colonial architecture, scenic views"
+  },
+  
+  {
+    name: "Udaipur",
+    media: "/src/assets/DestinationCards/Udaipur.mp4", // Image
+    description: "City of lakes, beautiful palaces and gardens"
+  },
+  {
+    name: "Darjeeling",
+    media: "/src/assets/DestinationCards/Darjirling.mp4", // Image
+    description: "Tea gardens, panoramic views of the Himalayas"
+  },
+  {
+    name: "Munnar",
+    media: "/src/assets/DestinationCards/Munnar.mp4", // Image
+    description: "Tea plantations, cool climate, Eravikulam National Park"
+  },
+  {
+    name: "Golden Temple, Amritsar",
+    media: "/src/assets/DestinationCards/Golden Temple.mp4", // Image
+    description: "Holiest shrine of Sikhism, spiritual experience"
+  },
+  {
+    name: "Sikkim",
+    media: "/src/assets/DestinationCards/Sikkim.mp4", // Image
+    description: "Snow-capped mountains, monasteries, trekking routes"
+  },
+  {
+    name: "Mount Abu",
+    media: "/src/assets/DestinationCards/Mount Abu.mp4", // Image
+    description: "Only hill station of Rajasthan, Dilwara Temples"
+  },
+  {
+    name: "Pondicherry",
+    media: "/src/assets/DestinationCards/Pondechery.mp4", // Image
+    description: "French colonial architecture, beaches, peaceful vibe"
+  },
+  {
+    name: "Agra Fort",
+    media: "/src/assets/DestinationCards/AgraFort.jpg", // Image
+    description: "Historical fort, UNESCO World Heritage site"
+  },
+  {
+    name: "Hampi",
+    media: "/src/assets/DestinationCards/Hampi.jpg", // Image
+    description: "Ancient ruins, temples, UNESCO World Heritage site"
+  },
+];
